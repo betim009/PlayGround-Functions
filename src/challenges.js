8bc4dc43329a9873754229ea59625a321cd3232c
Alberto <betim009@gmail.com>
2023-07-06 21:32:46 -0300

    stash

--- src/challenges.js
+++ src/challenges.js
@@ -3,21 +3,20 @@ function compareTrue(a, b) {
   // seu código aqui
   if (a === true && b === true) {
     return true;
-  } else {
-    return false;
   }
+  return false;
 }
 // Desafio 2
 function calcArea(base, height) {
   // seu código aqui
-  areaTriangle = (base * height) / 2;
+  const areaTriangle = (base * height) / 2;
   console.log(areaTriangle);
   return areaTriangle;
 }
 // Desafio 3
 function splitSentence(string) {
   // seu código aqui
-  var splits = string.split(' ');
+  let splits = string.split(' ');
   return splits;
 }
 // Desafio 4
@@ -25,13 +24,13 @@ function concatName(array) {
   // seu código aqui
   let first = array[0];
   let last = array[array.length - 1];
-  return last + ', ' + first;
+  return `${last}, ${first}`;
 }
 // Desafio 5
 function footballPoints(wins, ties) {
   // seu código aqui
-  wins = wins * 3;
-  ties = ties * 1;
+  wins *= 3;
+  ties *= 1;
   return wins + ties;
 }
 // Desafio 6
@@ -48,7 +47,7 @@ function highestCount(array) {
 
   for (const element of array) {
     if (maxNumber === element) {
-      contador = contador + 1;
+      contador += 1;
     }
   }
   return contador;
@@ -57,45 +56,44 @@ function highestCount(array) {
 function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
   if (mouse < cat1) {
-    cat1 = cat1 - mouse;
+    cat1 -= mouse;
   } else {
-    mouse = mouse - cat1;
+    mouse -= cat1;
   }
   if (mouse < cat2) {
-    cat2 = cat2 - mouse;
+    cat2 -= mouse;
   } else {
-    mouse = mouse - cat2;
+    mouse -= cat2;
   }
   if (cat1 === 0) {
-    cat1 = cat1 + 1;
+    cat1 += 1;
   }
   if (cat2 === 0) {
-    cat1 = cat1 + 1;
+    cat1 += 1;
   }
 
   if (cat1 > cat2) {
     return 'cat2';
-    //console.log('cat2')
-  } else if (cat2 > cat1) {
+    // console.log('cat2')
+  } if (cat2 > cat1) {
     return 'cat1';
-    //console.log('cat1');
-  } else {
-    return 'os gatos trombam e o rato foge';
+    // console.log('cat1');
   }
+  return 'os gatos trombam e o rato foge';
 }
 // Desafio 8
 function fizzBuzz(array) {
   // seu código aqui
   resultado = [];
   for (const element of array) {
-    if(element % 3 === 0 && element % 5 === 0){
-      resultado.push("fizzBuzz");
-    } else if(element % 3 === 0){
-      resultado.push("fizz");
-    } else if(element % 5 === 0){ 
-      resultado.push("buzz");
-    } else if (element % 3 != 0 && element % 5 != 0){
-      resultado.push("bug!");
+    if (element % 3 === 0 && element % 5 === 0) {
+      resultado.push('fizzBuzz');
+    } else if (element % 3 === 0) {
+      resultado.push('fizz');
+    } else if (element % 5 === 0) {
+      resultado.push('buzz');
+    } else if (element % 3 != 0 && element % 5 != 0) {
+      resultado.push('bug!');
     }
   }
   return resultado;
@@ -123,16 +121,16 @@ function decode(string) {
 function techList(array, name) {
   // seu código aqui
   let newArray = [];
-  if(array[0] != undefined){
+  if (array[0] != undefined) {
     for (const element of array.sort()) {
       newArray.push({
-        tech: element, name: name
-      })
+        tech: element, name,
+      });
     }
-  } else{
+  } else {
     return 'Vazio!';
   }
-  
+
   return newArray;
 }
 module.exports = {
